8c49d193deaadcdf017723d2d9b58b7f77370549
sawsenFattahi <sawsen.fattahi@gmail.com>
2025-01-30 00:26:21 +0100

    strengthen security using limit rating and helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
+import { RateLimiterMemory } from 'rate-limiter-flexible';
 
 import { AppModule } from '@um/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const rateLimiter = new RateLimiterMemory({ points: 5, duration: 60 }); // 5 requests per minute
+  app.use(helmet());
+  app.use(async (req, res, next) => {
+    try {
+      await rateLimiter.consume(req.ip);
+      next();
+    } catch {
+      res.status(429).send('Too Many Requests, please try again later.');
+    }
+  });
   app.setGlobalPrefix('api/v1');
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
